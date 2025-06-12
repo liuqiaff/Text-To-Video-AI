e0b931fc347c10cbd2670dfc64967e9481bbae1d
liuqiaff <36257425+liuqiaff@users.noreply.github.com>
2025-06-12 11:17:28 +0800

    Update audio_generator.py

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,7 +1,7 @@
 import edge_tts
 
 async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"zh-CN-YunjianNeural")
+    communicate = edge_tts.Communicate("人是为活着本身而活着，而不是为了活着之外的任何事物所活着。","zh-CN-YunjianNeural")
     await communicate.save(outputFilename)
 
 
